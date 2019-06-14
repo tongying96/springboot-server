d2c0ef50e86eebd1e571057d2a7bbb618fc41ee4
tongying <1572369179@qq.com>
2019-06-14 11:07:05 +0800

    测

--- src/main/java/com/example/springbootserver/SpringbootServerApplication.java
+++ src/main/java/com/example/springbootserver/SpringbootServerApplication.java
@@ -19,7 +19,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 //@EnableAdminServer
 public class SpringbootServerApplication {
-
+//
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootServerApplication.class, args);
 	}
